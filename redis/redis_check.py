bf934d767049266f335076ffa2c62e4d8e8d7fd9
R4v3zn <woo0nise@gmail.com>
2019-12-26 12:38:40 +0800

    redis

--- redis/redis_check.py
+++ redis/redis_check.py
@@ -4,6 +4,9 @@
 import redis
 import time
 import os
+import logging
+logging.basicConfig(level = logging.INFO,format = '[*] %(asctime)s - %(levelname)s : %(message)s')
+logger = logging.getLogger(__name__)
 
 host = "127.0.0.1"
 port = 6379
@@ -14,11 +17,11 @@ if __name__ == '__main__':
         r = redis.Redis(host=host, port=6379, password=password, socket_connect_timeout=1)
         try:
             r.ping()
-            print("[*] redis %s:%s running !"%(host,str(port),))
+            logging.info(" redis %s:%s running !"%(host,str(port),))
         except Exception as e:
-            print("[-] redis %s:%s stop, rebooting !"%(host,str(port),))
+            logging.error("redis %s:%s stop, rebooting !"%(host,str(port),))
             p = os.system("systemctl start redis")
-            print("[-] redis %s:%s stop, reboot ok !"%(host,str(port),))
+            logging.error("redis %s:%s stop, reboot ok !"%(host,str(port),))
         finally:
             r.close()
         time.sleep(2)
