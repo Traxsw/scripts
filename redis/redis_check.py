0de8483470a6293f01d1eebfdd00c066cb66e08f
R4v3zn <woo0nise@gmail.com>
2019-12-26 12:32:23 +0800

    redis

--- redis/redis_check.py
+++ redis/redis_check.py
@@ -11,14 +11,14 @@ password=""
 
 if __name__ == '__main__':
     while True:
+        r = redis.Redis(host=host, port=6379, password=password, socket_connect_timeout=1)
         try:
-            r = redis.Redis(host=host, port=6379, password=password, socket_connect_timeout=1)
             r.ping()
             print("[*] redis %s:%s running !"%(host,str(port),))
         except Exception as e:
             print("[-] redis %s:%s stop, rebooting !"%(host,str(port),))
-            p = os.system("systemctl restart redis")
+            p = os.system("systemctl start redis")
             print("[-] redis %s:%s stop, reboot ok !"%(host,str(port),))
         finally:
             r.close()
-        time.sleep(1)
+        time.sleep(2)
